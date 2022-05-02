8199e761ba52a514456c3c9a8de53e31a0d868e3
KhangNgo-guest <85405931+KhangNgo-guest@users.noreply.github.com>
2022-05-02 21:57:43 +0700

    Info&AppSetting UI add

---
+++ app/src/main/java/com/codesieucap/ueh_checkin/GuideAndIntroductionActivity.java
@@ -0,0 +1,14 @@
+package com.codesieucap.ueh_checkin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuideAndIntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_and_introduction);
+    }
+}
---
+++ app/src/main/java/com/codesieucap/ueh_checkin/PasswordChangeSuccess.java
@@ -0,0 +1,16 @@
+package com.codesieucap.ueh_checkin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class PasswordChangeSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_change_success);
+    }
+}
