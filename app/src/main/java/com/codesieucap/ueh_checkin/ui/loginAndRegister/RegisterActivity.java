8c2b2d934e2a9acd3c3c8750eb138e1e191ffdf1
TrucKhang <truckhang181001@gmail.com>
2022-05-04 23:10:28 +0700

    register account none validate

--- app/src/main/java/com/codesieucap/ueh_checkin/ui/loginAndRegister/RegisterActivity.java
+++ app/src/main/java/com/codesieucap/ueh_checkin/ui/loginAndRegister/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.codesieucap.ueh_checkin.ui.loginAndRegister;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,14 +9,23 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.codesieucap.ueh_checkin.R;
+import com.codesieucap.ueh_checkin.models.UserModel;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private TextView tv_agree;
     private Button btn_confirm_register;
+    private DatabaseReference mDatabase;
+    private EditText userName,userEmail,userPassword,userPasswordCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,10 @@ public class RegisterActivity extends AppCompatActivity {
 
         tv_agree = findViewById(R.id.textview_agree_with_policy);
         btn_confirm_register = findViewById(R.id.button_confirm_register);
+        userName = findViewById(R.id.registerEditTextUserName);
+        userEmail = findViewById(R.id.registerEditTextUserEmail);
+        userPassword = findViewById(R.id.registerEditTextUserPassword);
+        userPasswordCheck = findViewById(R.id.registerEditTextUserPasswordCheck);
 
         String htmlString="Đồng ý với <b>điều khoản và dịch vụ</b>";
         tv_agree.setText(Html.fromHtml(htmlString));
@@ -47,7 +62,54 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void startRegisterSuccessActivity() {
-        Intent intent = new Intent(RegisterActivity.this, RegisterSuccessActivity.class);
-        startActivity(intent);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        String key = mDatabase.child("User").push().getKey();
+        UserModel user = new UserModel(key,userEmail.getText().toString(),userPassword.getText().toString(),userName.getText().toString(),"HCM",true,123456,"123456asdfdsf");
+        mDatabase.child("User").child(key).setValue(user, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                if(error == null){
+                    Intent intent = new Intent(RegisterActivity.this, RegisterSuccessActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    //Hiển thị thông báo lỗi
+                }
+            }
+        });
+    }
+    private Boolean validateRegister(){
+        //true thay thế bằng các validate các editText của user nhập vào
+        if(true){
+            mDatabase.child("User").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    if(userEmail.getText().toString().equals(snapshot.getValue(UserModel.class).getEmail())){
+
+                    }
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+        return true;
     }
 }
