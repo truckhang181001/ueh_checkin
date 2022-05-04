fe059d3664076dc40060cd1de5c2773cf4f4aaf4
TrucKhang <truckhang181001@gmail.com>
2022-05-04 08:15:41 +0700

    testing fire base

--- app/src/main/java/com/codesieucap/ueh_checkin/MainActivity.java
+++ app/src/main/java/com/codesieucap/ueh_checkin/MainActivity.java
@@ -1,11 +1,14 @@
 package com.codesieucap.ueh_checkin;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.navigation.NavController;
@@ -20,6 +23,11 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.codesieucap.ueh_checkin.databinding.ActivityMainBinding;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,12 +38,14 @@ import event.EventAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fireBaseTesting();
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -49,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+    }
+
+    protected void fireBaseTesting(){
+        mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
 }
