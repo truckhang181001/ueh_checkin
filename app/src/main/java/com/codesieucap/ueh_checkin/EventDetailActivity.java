3662529284dab1e8f7d1725d554d991b920e7239
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2022-05-05 18:08:25 +0700

    'manage_participant'

--- app/src/main/java/com/codesieucap/ueh_checkin/EventDetailActivity.java
+++ app/src/main/java/com/codesieucap/ueh_checkin/EventDetailActivity.java
@@ -2,13 +2,33 @@ package com.codesieucap.ueh_checkin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.codesieucap.ueh_checkin.ui.participant.ParticipantListActivity;
 
 public class EventDetailActivity extends AppCompatActivity {
+
+    private Button btn_manage_participant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
+
+        btn_manage_participant = findViewById(R.id.button_manage_list_participant);
+        btn_manage_participant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startParticipantListActivity();
+            }
+        });
+    }
+
+    private void startParticipantListActivity() {
+        Intent intent = new Intent(EventDetailActivity.this, ParticipantListActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/codesieucap/ueh_checkin/ui/participant/ParticipantAdapter.java
@@ -0,0 +1,75 @@
+package com.codesieucap.ueh_checkin.ui.participant;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codesieucap.ueh_checkin.R;
+
+import java.util.List;
+
+public class ParticipantAdapter extends RecyclerView.Adapter<ParticipantAdapter.ParticipantViewHolder> {
+
+    private Context context;
+    private List<Participant> participantList;
+
+    public ParticipantAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(List<Participant> list){
+        this.participantList = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ParticipantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_participantlayout, parent, false);
+        return new ParticipantViewHolder(view);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onBindViewHolder(@NonNull ParticipantViewHolder holder, int position) {
+        Participant participant = participantList.get(position);
+        if (participant == null){
+            return;
+        }
+        holder.tv_oder.setText(participant.getOrder());
+        holder.tv_name.setText(participant.getName());
+        holder.tv_class.setText(participant.getBranch());
+        holder.tv_status.setText(participant.getStatus());
+        if (participant.getStatus() == "0") {
+            holder.tv_status.setBackgroundColor(R.color.yellow);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (participantList != null){
+            return participantList.size();
+        }
+        return 0;
+    }
+
+    public class ParticipantViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tv_oder, tv_name, tv_class, tv_status;
+
+        public ParticipantViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tv_oder = itemView.findViewById(R.id.participant_oder);
+            tv_name = itemView.findViewById(R.id.participant_name);
+            tv_class = itemView.findViewById(R.id.participant_class);
+            tv_status = itemView.findViewById(R.id.participant_status);
+        }
+    }
+}
