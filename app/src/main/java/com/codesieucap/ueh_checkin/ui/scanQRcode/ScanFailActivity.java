094dbc91ad2c0e330ca6aa5e4bcb46a845e5deaf
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2022-05-05 19:31:24 +0700

    'add_notifi_status_when_checkin'

---
+++ app/src/main/java/com/codesieucap/ueh_checkin/ui/scanQRcode/ScanFailActivity.java
@@ -0,0 +1,16 @@
+package com.codesieucap.ueh_checkin.ui.scanQRcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codesieucap.ueh_checkin.R;
+
+public class ScanFailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_fail);
+    }
+}
